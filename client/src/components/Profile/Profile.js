71bac13cc78866ff18f0153534862f6d2c1c2ce1
Diana <dianaliviz2001@gmail.com>
2021-10-24 02:04:27 +0300

    profile page

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,8 +1,25 @@
-let Profile = () => {
+import React from "react";
+import { POSTEXAMPLE } from "../BlogBlock/Post";
+import { USER, UserInfo } from "../UserInfo/UserInfo";
+import { Post } from "../BlogBlock/Post";
+import { Grid } from "@material-ui/core";
+
+const POSTS = [POSTEXAMPLE, POSTEXAMPLE, POSTEXAMPLE];
+
+const Profile = ({ posts } = {}) => {
+  posts = POSTS;
   return (
     <>
-      <p>Profile</p>
+      <UserInfo {...USER}/>
+      <Grid container spacing={2}>
+        {posts.map((post, index) => (
+          <Grid item>
+            <Post key={index} {...post} />
+          </Grid>
+        ))}
+      </Grid>
     </>
-  )
-}
+  );
+};
+
 export default Profile;
